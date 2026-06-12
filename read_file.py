302e44a374ff4121968785b855694be33348fe47
Shayhan Ameen Chowdhury <69597678+shayhan-ameen@users.noreply.github.com>
2026-06-12 16:29:22 +0900

    Enhance Documentation
    Enhance Documentation

--- read_file.py
+++ read_file.py
@@ -19,18 +19,24 @@ def read_csv(path: str, num_classes) -> tuple:
     # Read in dataset
     data = pd.read_csv(path, dtype={"Subject": str})
     if num_classes == "Folder":
-        data.loc[data['Filename'].str.contains('anger'), 'Estimated Emotion Folder'] = 'anger'
-        data.loc[data['Filename'].str.contains('disgust'), 'Estimated Emotion Folder'] = 'disgust'
-        data.loc[data['Filename'].str.contains('happy'), 'Estimated Emotion Folder'] = 'happy'
+        data.loc[data["Filename"].str.contains("anger"), "Estimated Emotion Folder"] = (
+            "anger"
+        )
+        data.loc[
+            data["Filename"].str.contains("disgust"), "Estimated Emotion Folder"
+        ] = "disgust"
+        data.loc[data["Filename"].str.contains("happy"), "Estimated Emotion Folder"] = (
+            "happy"
+        )
 
     emotion_categories = "Estimated Emotion " + str(num_classes)
     data.dropna(subset=[emotion_categories], inplace=True)
     data.reset_index(inplace=True)
 
-
     # Label the emotions into number
     label_mapping = {
-        emotion: idx for idx, emotion in enumerate(np.unique(data.loc[:, emotion_categories]))
+        emotion: idx
+        for idx, emotion in enumerate(np.unique(data.loc[:, emotion_categories]))
     }
     print(label_mapping)
 
